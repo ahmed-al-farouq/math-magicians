f23eafa731dfad29928597f89510f915cefe385a
ahmed-al-farouq <mediocre23534@gmail.com>
2021-10-17 08:28:48 -0400

    Make the required changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import React, { Component } from 'react';
 import Calculator from './components/Calculator';
+import './App.css';
 
-function App() {
-  return (
-    <Calculator />
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
