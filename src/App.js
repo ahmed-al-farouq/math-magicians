b35a74c8511ab623e09ddd16edee950d1f0c9099
ahmed-al-farouq <mediocre23534@gmail.com>
2021-10-18 07:39:46 -0400

    Add calculator functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import Calculator from './components/Calculator';
+import calculate from './logic/calculate';
 import './App.css';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
+  }
+
+  updateData = (e) => {
+    const { data } = this.state;
+    const newData = calculate(data, e.target.textContent);
+    this.setState({
+      data: newData,
+    });
+  }
+
   render() {
+    const { data } = this.state;
+    let value = '';
+    if (data.total) {
+      value = `${data.total} ${data.operation || ''} ${data.next || ''}`;
+    } else if (data.next) {
+      value = `${data.next} ${data.operation || ''}`;
+    }
+
     return (
-      <Calculator />
+      <Calculator updateData={this.updateData} value={value} />
     );
   }
 }
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,37 +1,44 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends Component {
   render() {
+    const { updateData, value } = this.props;
     return (
       <section className="calculator-container">
         <div className="output">
           <div className="operand">
-            0
+            {value || 0}
           </div>
         </div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="operators">÷</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="operators">&times;</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="operators">+</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="operators">-</button>
-        <button type="button" className="span-two">0</button>
-        <button type="button">.</button>
-        <button type="button" className="operators">=</button>
+        <button type="button" onClick={updateData}>AC</button>
+        <button type="button" onClick={updateData}>+/-</button>
+        <button type="button" onClick={updateData}>%</button>
+        <button type="button" onClick={updateData} className="operators">÷</button>
+        <button type="button" onClick={updateData}>7</button>
+        <button type="button" onClick={updateData}>8</button>
+        <button type="button" onClick={updateData}>9</button>
+        <button type="button" onClick={updateData} className="operators">x</button>
+        <button type="button" onClick={updateData}>4</button>
+        <button type="button" onClick={updateData}>5</button>
+        <button type="button" onClick={updateData}>6</button>
+        <button type="button" onClick={updateData} className="operators">+</button>
+        <button type="button" onClick={updateData}>1</button>
+        <button type="button" onClick={updateData}>2</button>
+        <button type="button" onClick={updateData}>3</button>
+        <button type="button" onClick={updateData} className="operators">-</button>
+        <button type="button" onClick={updateData} className="span-two">0</button>
+        <button type="button" onClick={updateData}>.</button>
+        <button type="button" onClick={updateData} className="operators">=</button>
       </section>
     );
   }
 }
+
+Calculator.propTypes = {
+  updateData: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default Calculator;
