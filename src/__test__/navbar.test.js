06681b133897cbd09c8f40e628d3e5438a15dded
Ahmed Al Farouq <mediocre23534@gmail.com>
2021-10-29 23:22:32 +0200

    Add testing snapshoots

--- src/__test__/navbar.test.js
+++ src/__test__/navbar.test.js
@@ -1,5 +1,15 @@
+import React from 'react';
 import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router';
+import Navbar from '../components/Navbar';
 
 it('Test navbar', () => {
-  const tree = renderer.create(<Navbar />).toJSON();
+  const tree = renderer
+    .create(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
